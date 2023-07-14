1b0e0945d4fe900ded0a8ff85fcbc3a562d37269
sam88mohd <sam88mohd@gmail.com>
2023-07-14 20:48:55 +0800

    initial commit

---
+++ settings.py
@@ -0,0 +1,15 @@
+# PostgreSQL
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'ODQ5MS1zYW04OG1v',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
+INSTALLED_APPS = (
+    'standalone',
+)
+SECRET_KEY = 'SECRET KEY for this Django Project'
